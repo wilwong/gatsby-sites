0e8f965dd6b70cb47ead73296525639e247f1a8f
dev <test@local.host>
2024-07-31 17:41:23 -0400

    better prefixing

--- gatsby-one/src/components/BlogRollItem.tsx
+++ gatsby-one/src/components/BlogRollItem.tsx
@@ -1,29 +1,31 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, withPrefix } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 export const BlogRollItem = ({postFrontmatter: pfm}:{postFrontmatter: any})=> {
   const featuredImage =
     pfm.featuredImage?.childImageSharp?.gatsbyImageData &&
       getImage(pfm.featuredImage.childImageSharp.gatsbyImageData);
 
-    return (
-      <li className="group">
-        <Link to={pfm.slug} title={pfm.title}>
-          <div className="h-32 overflow-hidden rounded-lg bg-panel/5 sm:h-52">
-            {featuredImage && (
-              <GatsbyImage
-                alt={`${pfm.title} featured image`}
-                image={featuredImage}
-                className="h-full w-full object-cover transition-all duration-[400ms] group-hover:scale-110"
-              />
-            )}
-          </div>
-          <div className="mt-3 flex flex-col">
-            <h4 className="my-0 text-[16px] transition-all group-hover:text-link md:text-[18px]">
-              {pfm.title}
-            </h4>
-          </div>
-        </Link>
-      </li>
-    );
+  const slug = withPrefix(pfm.slug);
+  
+  return (
+    <li className="group">
+      <Link to={slug} title={pfm.title}>
+        <div className="h-32 overflow-hidden rounded-lg bg-panel/5 sm:h-52">
+          {featuredImage && (
+            <GatsbyImage
+              alt={`${pfm.title} featured image`}
+              image={featuredImage}
+              className="h-full w-full object-cover transition-all duration-[400ms] group-hover:scale-110"
+            />
+          )}
+        </div>
+        <div className="mt-3 flex flex-col">
+          <h4 className="my-0 text-[16px] transition-all group-hover:text-link md:text-[18px]">
+            {pfm.title}
+          </h4>
+        </div>
+      </Link>
+    </li>
+  );
 }
--- gatsby-one/src/components/PostTemplate.tsx
+++ gatsby-one/src/components/PostTemplate.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { MDXProvider } from "@mdx-js/react";
-import { graphql, Link } from "gatsby";
+import { graphql, Link, withPrefix } from "gatsby";
 import type { HeadProps, PageProps } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
@@ -22,14 +22,14 @@ export default function PostTemplate({
   children,
 }: PageProps<Queries.PostTemplateQuery>) {
   const post = data.mdx;
-  // const featuredImage =
-  //   post?.frontmatter?.featuredImage?.childImageSharp?.gatsbyImageData &&
-  //   getImage(post.frontmatter.featuredImage.childImageSharp.gatsbyImageData);
+  const featuredImage =
+    post?.frontmatter?.featuredImage?.childImageSharp?.gatsbyImageData &&
+    getImage(post.frontmatter.featuredImage.childImageSharp.gatsbyImageData);
 
   return (
     <Layout>
       <Link
-        to="/"
+        to={withPrefix("/")}
         className="text-md flex w-fit items-center gap-1.5 font-semibold opacity-80 hover:underline"
       >
         Back to Blog
@@ -38,7 +38,7 @@ export default function PostTemplate({
         {post?.frontmatter?.title}
       </h1>
 
-      {/* <div className="h-[200px] w-full overflow-hidden rounded-lg bg-panel/10 sm:h-[300px] md:h-[400px] lg:h-[500px]">
+      <div className="h-[200px] w-full overflow-hidden rounded-lg bg-panel/10 sm:h-[300px] md:h-[400px] lg:h-[500px]">
         {featuredImage && (
           <GatsbyImage
             alt={`${post.frontmatter.title} featured image`}
@@ -46,7 +46,7 @@ export default function PostTemplate({
             className="h-full w-full object-cover"
           />
         )}
-      </div> */}
+      </div>
       <div className="mt-8 flex gap-28 lg:mt-10">
         <div className="mdx-styles group mb-20 grow">
           <MDXProvider
